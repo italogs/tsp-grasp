ab1a3d59dab24f4b80e0e95e51e0e185559dc38f
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2016-03-10 13:50:20 -0300

    Variable names for convenience were edited and comments added

--- README.md
+++ README.md
@@ -20,6 +20,27 @@ BEGIN
 	%%%========
 
 
+	Format:
+	./grasp <file> <alpha> <grasp_iterations> <RCL size> <local_search_type> > exit.txt
+
+	local_search_type:
+	TWO_OPT - 2-opt
+	SWAP - simple swap
+	To be developed:
+	THREE_OPT - 3-opt
+	OR_OPT - Or-Opt
+	LK - Lin Kernighan
+	CW - Clark Wright
+	CI - Cheapest Insertion
+	RI - Random Insertion
+
+	The file must contains the line 'DIMENSION:' before the first line of coordinates.
+	See dj38.tsp for basic example. The data before 'DIMENSION:' will be ignored.
+	To avoid unexpected behavior, add EOF at the end.
+
+	Example:
+	./grasp att48.tsp 0.0025 100 5 TWO_OPT > exit.txt
+
 	Incoming improvements.
 	Suggestions are welcomed.
 
--- main.cpp
+++ main.cpp
@@ -10,26 +10,27 @@
 #include "mtrand.h"
 using namespace std;
 
+
+//Local Optimization 
 enum {
 	SWAP, TWO_OPT
 };
 
-
+//Input data will be saved in this struct
 typedef struct graph {
 	float x,y;
 	int label;
 }graph;
 
+//Class for Solution
 class Solution{
 	private:
 		float cost;
-		int *circuit;
-		int tam_circuit;
+		int *circuit, size_circuit;
 	public:
-		Solution(int tam_circuit){
-			this->cost = 0;
-			this->circuit = new int[tam_circuit];
-			this->tam_circuit = 0;
+		Solution(int size_circuit){
+			this->size_circuit = this->cost = 0;
+			this->circuit = new int[size_circuit];
 		}
 		float getCost(){
 			return this->cost;
@@ -41,22 +42,22 @@ class Solution{
 			this->cost = new_cost;
 		}
 		void includeLabel(int label) {
-			circuit[tam_circuit++] = label;
+			circuit[size_circuit++] = label;
 		}
-		int getTam_circuit(){
-			return tam_circuit;
+		int getSize_circuit(){
+			return size_circuit;
 		}
-		void setTam_circuit(int tam_circuit){
-			this->tam_circuit = tam_circuit;
+		void setSize_circuit(int size_circuit){
+			this->size_circuit = size_circuit;
 		}
 		int getLastLabel(){
-			return circuit[tam_circuit-1];
+			return circuit[size_circuit-1];
 		}
 		int getLabelByIndex(int index){
-			if(index == tam_circuit)
+			if(index == size_circuit)
 				return circuit[0];
 			if(index < 0 )
-				return circuit[tam_circuit-1];
+				return circuit[size_circuit-1];
 			return circuit[index];
 		}
 		void setLabelByIndex(int index,int label){
@@ -66,21 +67,21 @@ class Solution{
 
 class Grasp {
 	private:
-		int MAX_ITERATIONS,tam_graph,tam_circuit,tam_LRC,local_search_type;
+		int MAX_ITERATIONS,size_graph,size_circuit,size_RCL,local_search_type;
 		Solution *best_solution;
 		graph *input;
 		MTRand drand;
 		time_t seconds;
 	public:
-		Grasp(int MAX_ITERATIONS,graph *input,int tam_graph, int tam_LRC,int local_search_type){
+		Grasp(int MAX_ITERATIONS,graph *input,int size_graph, int size_RCL,int local_search_type){
 			this->MAX_ITERATIONS = MAX_ITERATIONS;
 			best_solution = NULL;
 			this->input = input;
-			this->tam_graph = tam_graph;
-			this->tam_circuit=0;
+			this->size_graph = size_graph;
+			this->size_circuit=0;
 			time(&seconds);
 			drand.seed((unsigned int)seconds);
-			this->tam_LRC = tam_LRC;
+			this->size_RCL = size_RCL;
 			this->local_search_type = local_search_type;
 		}
 		~Grasp(){
@@ -106,55 +107,57 @@ class Grasp {
 		float calculateCost(int i,int j){
 			i--;
 			j--;
-			if(i < 0) i = tam_circuit-1;
-			if(j < 0) j = tam_circuit-1;
+			if(i < 0) i = size_circuit-1;
+			if(j < 0) j = size_circuit-1;
 			return sqrt( pow(input[i].x - input[j].x,2) + pow(input[i].y - input[j].y,2));
 		}
 		void printCircuit(Solution *solution){
-			for (int i = 0; i < solution->getTam_circuit(); i++){
+			for (int i = 0; i < solution->getSize_circuit(); i++){
 				cout<<solution->getLabelByIndex(i)<<"->";
 			}
 			cout<<endl;
 		}
 		void printCircuitLatex(){
 			float scale = 100.0;
-			for (int i = 0; i < best_solution->getTam_circuit(); i++){
+			for (int i = 0; i < best_solution->getSize_circuit(); i++){
 				cout<<"\\node[draw,circle] ("<<input[best_solution->getLabelByIndex(i)-1].label<<") at ("<<(input[best_solution->getLabelByIndex(i)-1].x/scale)<<","<<(input[best_solution->getLabelByIndex(i)-1].y/scale)<<"){"<<input[best_solution->getLabelByIndex(i)-1].label<<"};"<<endl;
 			}
-			for (int i = 0; i < tam_graph-1; i++){
+			for (int i = 0; i < size_graph-1; i++){
 				cout<<"\\draw[-,line width=0.1pt] ("<<best_solution->getLabelByIndex(i)<<") -- ("<<best_solution->getLabelByIndex(i+1)<<");"<<endl;
 			}
-			cout<<"\\draw[-,line width=0.1pt] ("<<best_solution->getLabelByIndex(tam_graph-1)<<") -- ("<<best_solution->getLabelByIndex(0)<<");"<<endl;
+			cout<<"\\draw[-,line width=0.1pt] ("<<best_solution->getLabelByIndex(size_graph-1)<<") -- ("<<best_solution->getLabelByIndex(0)<<");"<<endl;
 		}
 		Solution *Update_Cost(Solution *solution) {
 			solution->setCost(0);
-			for (int i = 0; i < solution->getTam_circuit()-1; i++) {
+			for (int i = 0; i < solution->getSize_circuit()-1; i++) {
 				solution->addCost(calculateCost(solution->getLabelByIndex(i),solution->getLabelByIndex(i+1)));
 			}
-			solution->addCost(calculateCost(solution->getLabelByIndex(solution->getTam_circuit()-1),solution->getLabelByIndex(0)));
+			solution->addCost(calculateCost(solution->getLabelByIndex(solution->getSize_circuit()-1),solution->getLabelByIndex(0)));
 			return solution;
 		}
 
 		Solution *Greedy_Randomized_Constrution(float alpha) {
-			Solution *partial_solution = new Solution(tam_graph+1);
+			Solution *partial_solution = new Solution(size_graph);
 			int counter_restrict_candidate_list = 0, selected_candidate;
-			int *restrict_candidate_list = new int[this->tam_LRC], *candidate_set = new int[tam_graph];;
+			int *restrict_candidate_list = new int[this->size_RCL], *candidate_set = new int[size_graph];;
 			float c_min, c_max, cost,cost_threshold;
 
-			for(int i=0;i<tam_graph;i++)
+			for(int i=0;i<size_graph;i++)
 				candidate_set[i] = input[i].label;
 			
-			//putting first vertex in the solution
+			//putting first vertex in the solution and set it to 'unavaiable'
 			partial_solution->includeLabel(candidate_set[0]);
 			candidate_set[0] = -1;
 
-			while(partial_solution->getTam_circuit() < tam_graph) {
-				c_min = INT_MAX;
-				c_max = INT_MIN;
+			while(partial_solution->getSize_circuit() < size_graph) {
+				c_min = INT_MAX;//+inf
+				c_max = INT_MIN;//-inf
 
-				for (int i = 1; i < tam_graph; i++){
+				for (int i = 1; i < size_graph; i++){
+
 					if(candidate_set[i] == -1) 
 						continue;
+
 					cost = calculateCost(partial_solution->getLastLabel(),candidate_set[i]);
 					if(cost > c_max)
 						c_max = cost;
@@ -165,8 +168,9 @@ class Grasp {
 				cost_threshold = c_min + alpha * (c_max - c_min);
 
 				counter_restrict_candidate_list = 0;
+
 				// fill the RCL
-				for (int i = 1; i < tam_graph && counter_restrict_candidate_list != this->tam_LRC; i++){
+				for (int i = 1; i < size_graph && counter_restrict_candidate_list != this->size_RCL; i++){
 					if(candidate_set[i] == -1)
 						continue;
 					cost = calculateCost(partial_solution->getLastLabel(),candidate_set[i]);
@@ -180,7 +184,7 @@ class Grasp {
 
 				candidate_set[selected_candidate-1] = -1;
 			}
-			//add cost from the last vertex to the first vertex
+			//add the cost from the last vertex to the first vertex
 			partial_solution->addCost( calculateCost(partial_solution->getLastLabel(),candidate_set[0]));
 			return partial_solution;
 		}
@@ -189,13 +193,13 @@ class Grasp {
 			int best_index, temp_label;
 			float best_cost,current_cost,i_cost,new_cost;
 
-			for (int i = 1; i < partial_solution->getTam_circuit(); i++) {
+			for (int i = 1; i < partial_solution->getSize_circuit(); i++) {
 				best_index = i;
 				best_cost = INT_MAX;
 				i_cost = 	calculateCost(partial_solution->getLabelByIndex(i),partial_solution->getLabelByIndex(i+1)) +
 							calculateCost(partial_solution->getLabelByIndex(i),partial_solution->getLabelByIndex(i-1));
 
-				for (int j = i+1; j < partial_solution->getTam_circuit(); j++) {
+				for (int j = i+1; j < partial_solution->getSize_circuit(); j++) {
 					temp_label = partial_solution->getLabelByIndex(j);
 					current_cost = i_cost + 
 								calculateCost(partial_solution->getLabelByIndex(j),partial_solution->getLabelByIndex(j+1)) + 
@@ -225,28 +229,25 @@ class Grasp {
 			partial_solution = Update_Cost(partial_solution);
 			return partial_solution;
 		}
-		Solution *Two_Opt(Solution *partial_solution){
-			// cout<<"Two_Opt"<<endl;
+		Solution *Two_Opt(Solution *partial_solution){//only a draft, not the final version of this function
 			int improve = 0;
 			float new_cost = 0;
 			Solution *new_solution = NULL, *temp = NULL;
 			while(improve < 20) {
 				float best_cost = partial_solution->getCost();
-				for (int i = 0; i < partial_solution->getTam_circuit()-1; i++) {
+				for (int i = 0; i < partial_solution->getSize_circuit()-1; i++) {
 
-					for (int j = i+1; j < partial_solution->getTam_circuit(); j++) {
+					for (int j = i+1; j < partial_solution->getSize_circuit(); j++) {
 						new_solution = NULL;
 						new_solution = Two_Opt_Swap(partial_solution,i,j);
 
 						new_cost = new_solution->getCost();
-						// cout<<new_cost<<endl;
 						if(new_cost < best_cost	){
 							improve = 0;
 							temp = partial_solution;
 							partial_solution = new_solution;
 							delete temp;
 							best_cost = new_cost;
-							// cout<<"melhorou"<<endl;
 						}
 					}
 				}
@@ -256,21 +257,19 @@ class Grasp {
 			return partial_solution;
 		}
 		Solution *Two_Opt_Swap(Solution *partial_solution,int i,int j){
-			Solution *new_solution = new Solution(tam_graph+1);
+			Solution *new_solution = new Solution(size_graph);
 	 		for ( int c = 0; c <= i - 1; c++ ) {
 	 			new_solution->setLabelByIndex(c,partial_solution->getLabelByIndex(c));
-	        	// new_tour.SetCity( c, tour.GetCity( c ) ); 
 	   		}
 	   		int dec = 0;
 		    for ( int c = i; c <= j; c++ ) {
 		    	new_solution->setLabelByIndex(c,partial_solution->getLabelByIndex(j-dec));
 		        dec++;
 		    }
-		    for ( int c = j + 1; c < partial_solution->getTam_circuit(); c++ ) {
+		    for ( int c = j + 1; c < partial_solution->getSize_circuit(); c++ ) {
 		    	new_solution->setLabelByIndex(c,partial_solution->getLabelByIndex(c));
-			    // new_tour.SetCity( c, tour.GetCity( c ) );
 			}
-			new_solution->setTam_circuit(tam_graph);
+			new_solution->setSize_circuit(size_graph);
 			new_solution = Update_Cost(new_solution);
 			return new_solution;
 		}
@@ -281,7 +280,6 @@ class Grasp {
 				partial_solution = Two_Opt(partial_solution);
 			return partial_solution;
 		}
-
 		void Update_Solution(Solution *partial_solution){
 			// if first solution or partial_solution is better than the best_solution then update
 			if(best_solution && partial_solution->getCost() < best_solution->getCost()){
@@ -303,14 +301,15 @@ int main(int argc, char *argv[]){
 	}
 	ifstream file(argv[1]);
 	float alpha = atof(argv[2]);
-	int times_execute = atoi(argv[3]), dimensions = 0,tam_LRC = atoi(argv[4]);
+	int times_execute = atoi(argv[3]), dimensions = 0,size_RCL = atoi(argv[4]);
 	int local_search_type;
 	if(strcmp(argv[5],"SWAP")==0) {
 		local_search_type = SWAP;
 	} else if(strcmp(argv[5],"TWO_OPT")==0) {
 		local_search_type = TWO_OPT;
 	} else {
-		printf("Local search not defined.\n");
+		printf("Please, define a local search to be executed. See README.md\n");
+		return 0;
 	}
 	string x,y,label,line;
 	while(file){
@@ -330,7 +329,7 @@ int main(int argc, char *argv[]){
 		myGraph[i].label = atoi(label.c_str());
 	}
 
-	Grasp g(times_execute,myGraph,dimensions,tam_LRC,local_search_type);
+	Grasp g(times_execute,myGraph,dimensions,size_RCL,local_search_type);
 	g.execute(alpha);
 
 	cout<<"Best Solution: "<<g.getCostBestSolution()<<endl;
